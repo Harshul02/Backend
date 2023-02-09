a6b729eb86193e521c8396faf7b609fc80afe5ce
Harshul Jain <harshulgangwal02@gmail.com>
2023-02-09 10:51:42 +0530

    Add MiddleWares to Accept body and json format

--- Day 3(Auth without DB)/index.js
+++ Day 3(Auth without DB)/index.js
@@ -4,6 +4,15 @@ const app = express();
 
 const PORT = 5000;
 
+//MiddleWare    --Starting
+
+//accept json
+app.use(express.json());
+//accept body
+app.use(express.urlencoded({extended: true}));
+
+//MiddleWare    --Ending
+
 app.listen(PORT, ()=>{
     console.log(`Server Running at ${PORT}`);
 })
